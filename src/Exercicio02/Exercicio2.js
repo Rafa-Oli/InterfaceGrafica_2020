5a4cbe85163d003f3a148f5cf4bb09fb89e8535d
unknown <rafaela.albaniza@gmail.com>
2020-08-26 19:58:44 -0300

    exercicio 2

---
+++ src/Exercicio02/Exercicio2.js
@@ -0,0 +1,12 @@
+function verificarMaior(){
+    var numeros = document.getElementById("textNumber").value;
+    numeros = numeros.split(",");
+    var maiorValor=0;
+    for (i = 0; i < numeros.length; i++) {
+       if(numeros[i] > maiorValor){
+           maiorValor= parseInt(numeros[i]);
+       }
+       
+    }
+alert("O maior valor é " + maiorValor);
+}
