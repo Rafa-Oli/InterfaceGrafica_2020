84929fa0eaeee1e0e566cbc1d64e0a7af66cfede
unknown <rafaela.albaniza@gmail.com>
2020-08-26 20:00:32 -0300

    exercicio 2

--- src/Exercicio02/Exercicio2.js
+++ src/Exercicio02/Exercicio2.js
@@ -1,12 +1,12 @@
-function verificarMaior(){
+function verificarMaior() {
     var numeros = document.getElementById("textNumber").value;
     numeros = numeros.split(",");
-    var maiorValor=0;
+    var maiorValor = 0;
     for (i = 0; i < numeros.length; i++) {
-       if(numeros[i] > maiorValor){
-           maiorValor= parseInt(numeros[i]);
-       }
-       
+        if (numeros[i] > maiorValor) {
+            maiorValor = parseInt(numeros[i]);
+        }
+
     }
-alert("O maior valor é " + maiorValor);
+    alert("O maior valor é " + maiorValor);
 }
