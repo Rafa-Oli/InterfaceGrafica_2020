06d89eb91695a2a6a816ef208b85b36ef878575c
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-08-31 11:18:53 -0300

    exercicio 7

--- src/Exercicio07/Exercicio7.js
+++ src/Exercicio07/Exercicio7.js
@@ -1,12 +1,12 @@
 function SomarImpares() {
     var numeros = document.getElementById("textNumber").value;
     numeros = numeros.split(",");
-    var somaImapares = 0;
+    var somaImpares = 0;
 
     for (i = 0; i < numeros.length; i++) {
         if ((!(parseInt(numeros[i]) % 2 === 0)) && (parseInt(numeros[i]) % 3 === 0)) {
-            somaImapares= somaImapares + parseInt(numeros[i]);
+            somaImpares = somaImpares + parseInt(numeros[i]);
         }
     }
-    alert("A soma dos impares é : " + somaImapares);
+    alert("A soma dos impares é : " + somaImpares);
 }
