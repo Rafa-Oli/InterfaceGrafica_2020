74937bef5c35ce7755dff076e835ed57c8ecbbf3
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-10-06 15:48:26 -0300

    js

--- src/Q2.js
+++ src/Q2.js
@@ -1,14 +1,27 @@
-const pessoa = {
-    id: 1,
-    nome: '',
-    n: 4,
-    a1: 2,
-    r: 6
+function progAritmetica({ n, a1, r }) {
+    console.log('Elementos: ')
+    soma = 0
+    for (let i = 1; i <= n; i++) {
+        console.log(a1)
+        soma += a1
+        a1 = a1 + r
+    }
+    console.log('Soma dos elementos: ' + soma)
 }
 
-function progAritmetica(obj){
+function progGeometrica({ n, a1, r }) {
+    console.log('Elementos: ')
+    soma = 0
+    for (let i = 1; i <= n; i++) {
+        console.log(a1)
+        soma += a1
+        a1 = a1 * r
+    }
+    console.log('Soma dos elementos: ' + soma)
+}
 
-    
-
-
-} 
+const obj = { id: 1, nome: 'Progressao', n: 10, a1: 4, r: 2 }
+console.log('Progressão Aritmética: ')
+progAritmetica(obj)
+console.log('\nProgressão Geomética: ')
+progGeometrica(obj)
