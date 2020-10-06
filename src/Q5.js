a966183594968d6667099f68df73768f52ff4786
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-10-05 22:22:31 -0300

    js

--- src/Q5.js
+++ src/Q5.js
@@ -1,18 +1,23 @@
 
-function isPrime({inicio = 0,fim= 100}) {
-    let texto= '';
-    for(let count = inicio ; count<= fim ; count++){
-        divisores=0;
-        for(var aux=1 ; aux<=count ; aux++)
-            if(count % aux == 0)
+function isPrime({ inicio = 0, fim = 100 }) {
+    let texto = '';
+    for (let count = inicio; count <= fim; count++) {
+        divisores = 0;
+        for (var aux = 1; aux <= count; aux++)
+            if (count % aux == 0) 
                 divisores++;
-  
-        if(divisores==2){
-            texto += count + ' '};
-    }
-    
-   return texto
-  }
 
-  
-  console.log(isPrime({inicio: 40,fim: 70}))
+        if (divisores == 2) { // verificando se a quatidade de divisores(1 ou por ele msm) 
+            texto += count + ' '
+        };
+    }
+
+    return texto
+}
+
+
+console.log(isPrime({ inicio: 20, fim: 40 }))
+
+
+
+
