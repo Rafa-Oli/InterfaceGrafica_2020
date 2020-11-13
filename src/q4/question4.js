862d4a0595a014bbf3d8e9bc7660e7cb9538d868
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-11-13 10:26:51 -0300

    refactor: change functions to create Promise

--- src/q4/question4.js
+++ src/q4/question4.js
@@ -1,33 +1,46 @@
 
-function colocarTodasPalavrasEmMaiusculo(palavras){
-    return new Promise(function(resolve,reject){
-      try{
-        resolve(palavras.map().map(e => e.toUpperCase()))
-        }catch(e){
 
-            reject('Ocorreu um erro')
-        }
+const array = ['javascript', 'java', 'dart', 'python']
+
+
+
+ function colocarTodasPalavrasEmMaiusculo(array) {
+ return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            for (let i = 0; i < array.length; i++) {
+                if (typeof array[i] === "number") {
+                    reject(`Ocorreu um erro: valor não é string`)
+                }
+            }
+            resolve(array.map(e => {
+                if (typeof e === "string") {
+                    return e.toUpperCase()
+                }
+            })
+            )
+        }, 3000)
+
+    }
+    )
+}
+function ordenarPalavras(array) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try{
+            array.forEach(element => {
+                if (typeof element === "number") {
+                    reject("Ocorreu um erro: valor não é string")
+                }
+            });
+            resolve(array.sort())
+
+         }catch(e){
+             console.log(e)
+         } }, 1000)
     })
 }
-const palavras= ['javascript', 'java', 'dart', 'python',1]
-
-colocarTodasPalavrasEmMaiusculo(palavras)
-.then(console.log)
-.catch(erro => console.log(erro))
-
-
-function ordenarPalavras(){
-    return new Promise(function (resolve, reject) {
-        try {
-            resolve(palavras.map(e => e.toUpperCase()))
-        } catch (e) {
-
-            reject('Ocorreu um erro')
-        }
-    })
-}
 
 
 
-
-
+colocarTodasPalavrasEmMaiusculo(array).then(console.log).catch(console.log)
+ordenarPalavras(array).then(console.log).catch(console.log)
